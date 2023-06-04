96af7b5bf5c06d1fbb289ad26990a1f9f06c2e73
Kate_M <kate.maistrenko22@bathspa.ac.uk>
2023-06-04 18:57:24 +0100

    Simple String Hash + Tests

---
+++ Algorithms/Algorithms/SimpleHash.cs
@@ -0,0 +1,24 @@
+﻿namespace Kate.Algorithms
+{
+	public class SimpleHash
+	{
+		public static long CalculateSimpleStrHash(string str)
+		{
+			const int p = 31;
+			const long m = 1000000009;
+			long hash = 0;
+			long k = 1;
+			for (int i = 0; i < str.Length; i++)
+			{
+				hash += ConvertCharToInt(str[i])*k;
+				k = p*k%m;
+			}
+			return hash;
+		}
+
+		private static int ConvertCharToInt(char v)
+		{
+			return v - 'a' + 1;
+		}
+	}
+}
---
+++ Algorithms/TestProject/SimpleHashTest.cs
@@ -0,0 +1,31 @@
+﻿using Kate.Algorithms;
+
+namespace Kate.TestProject;
+
+[TestClass]
+public class SimpleHashTest
+{
+	[TestMethod]
+	public void Calculate_TheSameInput_TheSameOutput()
+	{
+		Assert.AreEqual(SimpleHash.CalculateSimpleStrHash("kate"), SimpleHash.CalculateSimpleStrHash("kate"));
+	}
+
+	[TestMethod]
+	public void Calculate_DifferentInput_DifferentOutput()
+	{
+		Assert.AreNotEqual(SimpleHash.CalculateSimpleStrHash("ira"), SimpleHash.CalculateSimpleStrHash("kate"));
+	}
+
+	[TestMethod]
+	public void Calculate_ZeroInput_ZeroOutput()
+	{
+		Assert.AreEqual(0, SimpleHash.CalculateSimpleStrHash(""));
+	}
+
+	[TestMethod]
+	public void Calculate_abString()
+	{
+		Assert.AreEqual(63, SimpleHash.CalculateSimpleStrHash("ab"));
+	}
+}
